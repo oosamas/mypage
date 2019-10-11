f106909e2e52d0be8b8f3181333cd853de73d5d1
Osama Saddique <osaddique@gmail.com>
2019-10-11 14:05:19 -0400

    bug fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
   # get 'static_pages/home'
-  mount PdfjsViewer::Rails::Engine => "/pdfjs", as: 'pdfjs'
+  # mount PdfjsViewer::Rails::Engine => "/pdfjs", as: 'pdfjs'
   get 'static_pages/resume'
   get 'static_pages/contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
