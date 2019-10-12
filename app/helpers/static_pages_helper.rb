383714e0da61bb45c21a18ec119c33654d9272ba
Osama Saddique <osaddique@gmail.com>
2019-10-12 11:29:55 -0400

    add ico,text to fas icons

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,10 +1,3 @@
 module StaticPagesHelper
-  def full_title(page_title = "")
-    base_title = "Osama Saddique"
-    if page_title.empty?
-      base_title
-    else
-      page_title
-    end
-  end
+  
 end
